e51dc3a05ed2cb582722fcd6a501ca918c7e5da1
Bhavesh Anand Bajpai <bhaveshab01@gmail.com>
2023-03-12 22:01:19 +0530

    Add files via upload

---
+++ Candies and Two Sisters.py
@@ -0,0 +1,12 @@
+i = int(input())
+
+for x in range (i):
+     y = int(input())
+     if (y%2==0):
+         print((y-1)//2)
+     else:
+         print(y//2)
+     
+
+         
+                                   
